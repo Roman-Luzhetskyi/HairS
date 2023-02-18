db74245bf0d7318320c8bd11c68cf8d88ae8c1b5
Roman-Luzhetskyi <113299200+Roman-Luzhetskyi@users.noreply.github.com>
2023-02-18 19:38:16 +0100

    02-video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,23 +1,24 @@
-import { galleryItems } from './gallery-items'; 
-import SimpleLightbox from "simplelightbox";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
-import "simplelightbox/dist/simple-lightbox.min.css";
+// console.log(galleryItems);
 
-console.log(galleryItems);
+// const galleryDiv = document.querySelector(".gallery");
+// const galleryCard = createGalleryCards(galleryItems);
+// galleryDiv.insertAdjacentHTML('afterbegin', galleryCard);
 
-const galleryDiv = document.querySelector(".gallery");
-const galleryCard = createGalleryCards(galleryItems);
-galleryDiv.innerHTML = galleryCard;
+// function createGalleryCards(images) {
+//     return images.map(image => 
+// `<a class="gallery__item" href="${image.original}" onclick="return false;">
+// <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+// </a>`
+// ).join('');
+// }
 
-function createGalleryCards(images) {
-    return images.map(image => 
-`<a class="gallery__item" href="${image.original}" onclick="return false;">
-<img class="gallery__image" src="${image.preview}" alt="${image.description}" />
-</a>`
-).join('');
-}
-
-const lightbox = new SimpleLightbox(".gallery a", {
-    captionDelay: 250,
-    captionsData: "alt"
-});
+// const lightbox = new SimpleLightbox(".gallery a", {
+//     captionDelay: 250,
+//     captionsData: "alt"
+// });
