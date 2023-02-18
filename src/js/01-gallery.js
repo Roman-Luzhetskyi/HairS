7fca9c00b9e1db29b3f28860a79085de9dc629cc
Roman-Luzhetskyi <113299200+Roman-Luzhetskyi@users.noreply.github.com>
2023-02-18 15:55:25 +0100

    01-gallery.js

---
+++ src/js/01-gallery.js
@@ -0,0 +1,22 @@
+import { galleryItems } from './gallery-items'; 
+import SimpleLightbox from "simplelightbox"; 
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+
+const galleryDiv = document.querySelector(".gallery");
+const galleryCard = createGalleryCards(galleryItems);
+galleryDiv.innerHTML = galleryCard;
+
+function createGalleryCards(images) {
+    return images.map(image => 
+`<a class="gallery__item" href="${image.original}" onclick="return false;">
+<img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+).join('');
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt"
+});
